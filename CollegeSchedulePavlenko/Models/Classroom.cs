5597c8e6c86ff854405583ef994af54f39326686
JustChel883 <horeksid@mail.ru>
2026-03-16 22:37:45 +0700

    added code to Building.cs and Classroom.cs classes

--- CollegeSchedulePavlenko/Models/Classroom.cs
+++ CollegeSchedulePavlenko/Models/Classroom.cs
@@ -1,6 +1,22 @@
-﻿namespace CollegeSchedulePavlenko.Models
+﻿using CollegeSchedulePavlenko.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace CollegeSchedulePavlenko.Models
 {
+    [Table("classroom")]
     public class Classroom
     {
+        [Key]
+        [Column("classroom_id")]
+        public int ClassroomId { get; set; }
+        [Column("building_id")]
+        public int BuildingId { get; set; }
+        [ForeignKey("BuildingId")]
+        public Building Building { get; set; } = null!;
+        [Column("room_number")]
+        [Required]
+        public string RoomNumber { get; set; } = null!;
     }
 }
