768f589f18d548b4c2ea5f0060ddda65b904d741
JustChel883 <horeksid@mail.ru>
2026-03-16 23:22:57 +0700

    The service is registered in Program.cs

--- CollegeSchedulePavlenko/Program.cs
+++ CollegeSchedulePavlenko/Program.cs
@@ -2,7 +2,9 @@ using CollegeSchedulePavlenko.Middlewares;
 using CollegeSchedulePavlenko.Services;
 using CollegeSchedulePavlenko.Data;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 DotNetEnv.Env.Load();
 var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
@@ -13,6 +15,9 @@ var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" 
 $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
 builder.Services.AddDbContext<AppDbContext>(options =>
  options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IScheduleService, ScheduleService>();
+
 builder.Services.AddScoped<IScheduleService, ScheduleService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at
